56d8d0f085aba6016673615d49d4550ccee9f2e6
Stefan Amariei <as.stefanama@gmail.com>
2021-07-31 23:55:45 +0200

    Implementation of an algorithm that given an array counts the number of inversions. Formally speaking, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j. If the array is already sorted, then the inversion count is 0.

---
+++ sorting_problems/count_inversions.py
@@ -0,0 +1,44 @@
+arr = [8, 4, 2, 1]
+inversions = 0
+
+
+def merge(array, first, mid, last):
+    arr1 = [0] * ((mid - first) + 1)
+    i = 0
+    k = first
+    while i < len(arr1):
+        arr1[i] = array[k]
+        i += 1
+        k += 1
+    arr2 = [0] * (last - mid)
+    k = mid + 1
+    j = 0
+    while j < len(arr2):
+        arr2[j] = array[k]
+        j += 1
+        k += 1
+    i = 0
+    j = 0
+    k = first
+    while i < len(arr1) or j < len(arr2):
+        if j < len(arr2) and (i == len(arr1) or arr2[j] <= arr1[i]):
+            array[k] = arr2[j]
+            global inversions
+            inversions += len(arr1) - i
+            j += 1
+        else:
+            array[k] = arr1[i]
+            i += 1
+        k += 1
+
+
+def merge_sort(array, first, last):
+    if first < last:
+        mid = int((first + last) / 2)
+        merge_sort(array, first, mid)
+        merge_sort(array, mid + 1, last)
+        merge(array, first, mid, last)
+
+
+merge_sort(arr, 0, len(arr) - 1)
+print("The number of inversions are: " + str(inversions))
